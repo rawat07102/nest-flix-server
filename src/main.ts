a5c19a51508a07c1216ddbfbc14331aaf86f6125
rawat07102 <ritikrawat0710@gmail.com>
2020-05-15 12:38:14 +0530

    remove .env

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
+  app.enableCors({ credentials: true, origin: process.env.ORIGIN || '*' });
   app.use(cookieParser());
   app.useGlobalPipes(
     new ValidationPipe({
