554ec945bdb4d2631607ec53061d961d74b11f39
rawat07102 <ritikrawat0710@gmail.com>
2020-05-18 20:53:32 +0530

    add logout route and cookie to http-only

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,16 @@
-import { Controller, Post, Req, UseGuards, Res } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Req,
+  UseGuards,
+  Res,
+  HttpStatus,
+} from '@nestjs/common';
 import { LocalAuthGuard } from './guard/local.auth-guard';
 import { AuthService } from './auth.service';
 import { IRequestWithUser } from './types/RequestWithUser';
 import { Response } from 'express';
+import { JwtAuthGuard } from './guard/jwt.auth-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -12,7 +20,14 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   login(@Req() req: IRequestWithUser, @Res() res: Response) {
     const { access_token } = this.authService.login(req.user);
-    res.cookie('jwt', access_token, { sameSite: 'none' });
+    res.cookie('jwt', access_token, { sameSite: 'none', httpOnly: true });
     return res.json({ access_token });
+  }
+
+  @Post('logout')
+  @UseGuards(JwtAuthGuard)
+  logout(@Res() res: Response) {
+    res.clearCookie('jwt');
+    res.status(HttpStatus.CREATED).json(true);
   }
 }
