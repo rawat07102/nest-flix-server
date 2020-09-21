351a96bfe4a796b3ddaa26d4a286b8eaf5c123aa
rawat07102 <ritikrawat0710@gmail.com>
2020-09-21 13:40:20 +0530

    removed cookie usage for jwt to auth header

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,8 @@
-import {
-  Controller,
-  Post,
-  Req,
-  UseGuards,
-  Res,
-  HttpStatus,
-} from '@nestjs/common';
+import { Controller, Post, Req, UseGuards, Res } from '@nestjs/common';
 import { LocalAuthGuard } from './guard/local.auth-guard';
 import { AuthService } from './auth.service';
 import { IRequestWithUser } from './types/RequestWithUser';
 import { Response } from 'express';
-import { JwtAuthGuard } from './guard/jwt.auth-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -20,20 +12,6 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   login(@Req() req: IRequestWithUser, @Res() res: Response) {
     const { access_token } = this.authService.login(req.user);
-    res.cookie('jwt', access_token, {
-      sameSite: 'none',
-      httpOnly: true,
-      secure: !!process.env.SECURE_COOKIE,
-      path: '/',
-      domain: process.env.DOMAIN || '',
-    });
-    return res.json({ userId: req.user.id });
-  }
-
-  @Post('logout')
-  @UseGuards(JwtAuthGuard)
-  logout(@Res() res: Response) {
-    res.clearCookie('jwt');
-    res.status(HttpStatus.CREATED).json(true);
+    return res.json({ userId: req.user.id, access_token });
   }
 }
--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,22 +3,14 @@ import { Strategy, StrategyOptions, ExtractJwt } from 'passport-jwt';
 import { IPayload } from '../types/payload';
 import { UserDTO } from 'src/user/dto/user.dto';
 import { Injectable } from '@nestjs/common';
-import { Request } from 'express';
-
-export const extractFromCookie = (req: Request) => {
-  let token = null;
-  if (req && req.cookies) {
-    token = req.cookies['jwt'];
-  }
-  return token;
-};
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       secretOrKey: process.env.JWT_SECRET,
-      jwtFromRequest: extractFromCookie,
+      //* extract from auth header
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     } as StrategyOptions);
   }
 
